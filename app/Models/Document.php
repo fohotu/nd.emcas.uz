2e7a8478f4f62c35e8583d5687567836da9e4439
Emcas <f.pulatov@unicon.uz>
2026-03-31 16:20:38 +0500

    Created Document Test

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
 
 class Document extends Model
 {
+    use HasFactory, Notifiable;
     protected $guarded = []; 
 }
